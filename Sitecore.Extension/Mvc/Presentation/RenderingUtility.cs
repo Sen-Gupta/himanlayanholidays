10b2d772295f6c2e9bed2f149b138fa606b5a158
Sen <gupta.sen@hotmail.com>
2018-03-22 14:21:52 +0530

    Rendering Parameter

---
+++ Sitecore.Extension/Mvc/Presentation/RenderingUtility.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Data.Items;
+using Sitecore.Data.Fields;
+
+namespace Sitecore.Extension.Mvc.Presentation
+{
+    public static class RenderingUtility
+    {
+        public static string HideImage(this Rendering theRendering)
+        {
+            //Rendering Field is hard coded to  "hideImage"
+            //read value of HideImage Rendering paramter
+            string hide = theRendering.Parameters["hideImage"];
+
+            if(hide == "1")
+            {
+                Item theSetting = Sitecore.Context.Database.GetItem("{A5585EFC-DF8D-409C-A1ED-0D86FE26EE96}");
+                Sitecore.Diagnostics.Assert.ArgumentNotNull(theSetting, "item");
+                return theSetting["Setting Name"];
+            }
+            else
+            {
+                return string.Empty;
+            }
+            
+        }
+        public static string BackgroundColor(this Rendering theRendering)
+        {
+            //Rendering Field is hard coded to  "hideImage"
+            //read value of HideImage Rendering paramter
+            string theSelectedColor = theRendering.Parameters["backgroundColor"];
+            if(!String.IsNullOrEmpty(theSelectedColor))
+            {
+                Item theBackgroundColor = Sitecore.Context.Database.GetItem(theRendering.Parameters["backgroundColor"]);
+                Sitecore.Diagnostics.Assert.ArgumentNotNull(theBackgroundColor, "item");
+                return theBackgroundColor["Setting Name"];
+
+            }
+            else
+            {
+                return string.Empty;
+            }
+
+           
+            
+          
+            
+
+        }
+    }
+}
---
+++ Sitecore.ICT.Framework/Renderings/RenderingParameterUtility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+
+namespace Sitecore.ICT.Framework.Renderings
+{
+    public static class RenderingParameterUtility
+    {
+        public static string hideImage(string hide)
+        {
+            if(hide == "1")
+            {
+                Item theSetting = Sitecore.Context.Database.GetItem("{A5585EFC-DF8D-409C-A1ED-0D86FE26EE96}");
+                Sitecore.Diagnostics.Assert.ArgumentNotNull(theSetting, "item");
+                return theSetting["Setting Name"];
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+
+    
+}
