18586f30b4c67946512be0280737bcc07ddb020e
Sen <gupta.sen@hotmail.com>
2018-03-22 20:01:57 +0530

    Controller Rendering

---
+++ HimalayanHolidays/Controllers/PrimaryNavigationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Data.Items;
+
+
+
+namespace HimalayanHolidays.Controllers
+{
+    public class PrimaryNavigationController : Controller
+    {
+        // GET: PrimaryNavigation
+        public ActionResult Index()
+        {
+            
+            Item homeItem = Sitecore.Context.Database.GetItem(Sitecore.Context.Site.StartPath);
+            List<Item> ancestors = Sitecore.Context.Item.Axes.GetAncestors().ToList();
+            ancestors.Add(Sitecore.Context.Item);
+
+            ancestors.RemoveRange(0, ancestors.FindIndex(x => x.ID == homeItem.ID));
+
+             ancestors = ancestors.FindAll(x=>x["include in Navigation"] == "1").ToList();
+           
+            //Get list of all the ancestors 
+            
+            return View(ancestors);
+        }
+        
+    }
+}
