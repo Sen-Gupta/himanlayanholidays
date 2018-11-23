dea16bcc13caa987b80bd062f350ad00fcb0d3a3
sudhir <sudhir@ICAN-23>
2018-11-23 13:04:43 +0530

    Commiting changes

--- HimalayanHolidays/Controllers/PrimaryNavigationController.cs
+++ HimalayanHolidays/Controllers/PrimaryNavigationController.cs
@@ -12,21 +12,21 @@ namespace HimalayanHolidays.Controllers
     public class PrimaryNavigationController : Controller
     {
         // GET: PrimaryNavigation
-        public ActionResult Index()
-        {
+        //public ActionResult Index()
+        //{
             
-            Item homeItem = Sitecore.Context.Database.GetItem(Sitecore.Context.Site.StartPath);
-            List<Item> ancestors = Sitecore.Context.Item.Axes.GetAncestors().ToList();
-            ancestors.Add(Sitecore.Context.Item);
+        //    Item homeItem = Sitecore.Context.Database.GetItem(Sitecore.Context.Site.StartPath);
+        //    List<Item> ancestors = Sitecore.Context.Item.Axes.GetAncestors().ToList();
+        //    ancestors.Add(Sitecore.Context.Item);
 
-            ancestors.RemoveRange(0, ancestors.FindIndex(x => x.ID == homeItem.ID));
+        //    ancestors.RemoveRange(0, ancestors.FindIndex(x => x.ID == homeItem.ID));
 
-             ancestors = ancestors.FindAll(x=>x["include in Navigation"] == "1").ToList();
+        //     ancestors = ancestors.FindAll(x=>x["include in Navigation"] == "1").ToList();
            
-            //Get list of all the ancestors 
+        //    //Get list of all the ancestors 
             
-            return View(ancestors);
-        }
+        //    return View(ancestors);
+        //}
         
     }
 }
